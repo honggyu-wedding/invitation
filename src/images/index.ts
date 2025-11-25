ec8d9f1320d14e0bab81fd9637a4db43d72a80c4
honggyu-wedding <hyjack04@naver.com>
2025-11-25 22:55:08 +0900

    feat: Update project and replace images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,20 @@
-import coverImage from "./KJK_1815.jpg"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import coverImage from "./cover_image.jpg"
+import image1 from "./image01.jpg"
+import image2 from "./image02.jpg"
+import image3 from "./image03.HEIC"
+import image4 from "./image04.jpg"
+import image5 from "./image05.jpg"
+import image6 from "./image06.jpg"
+import image7 from "./image07.jpg"
+import image8 from "./image08.HEIC"
+import image9 from "./image09.jpg"
+import image10 from "./image10.jpg"
+import image11 from "./image11.JPG"
+import image12 from "./image12.HEIC"
+import image13 from "./image13.jpg"
+import image14 from "./image14.jpg"
+import image15 from "./image15.JPG"
+import image16 from "./image16.jpg"
 
 export const COVER_IMAGE = coverImage
 
@@ -27,4 +31,9 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
 ]
+
